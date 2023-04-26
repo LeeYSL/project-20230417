b16dd2d3ad05f53902ab3775a91ebb2dee60dca9
jeongjuri <jjeongjuri@naver.com>
2023-04-26 16:52:11 +0900

    컨트롤러

---
+++ pro/src/main/java/controller111/BoardController.java
@@ -0,0 +1,32 @@
+package controller111;
+
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+import gdu.mskim.MskimRequestMapping;
+import gdu.mskim.RequestMapping;
+
+@WebServlet(urlPatterns = {"/board/*"}, initParams = { @WebInitParam(name = "view", value="/veiw/") })
+public class BoardController extends MskimRequestMapping {
+	
+	@RequestMapping("boardList")
+	public String goboardList() {
+		return "board/boardList";
+		
+	}
+	@RequestMapping("writeForm")
+	public String gowriteForm() {
+		return "board/writeForm";
+		
+	}
+	
+	@RequestMapping("boardInfo")
+	public String goboardInfo() {
+		return "board/boardInfo";
+		
+	}
+	
+
+
+}
+
---
+++ pro/src/main/java/controller111/MarketController.java
@@ -0,0 +1,26 @@
+package controller111;
+
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+import gdu.mskim.MskimRequestMapping;
+import gdu.mskim.RequestMapping;
+
+@WebServlet(urlPatterns = { "/market/*" }, initParams = { @WebInitParam(name = "view", value = "/view/") })
+public class MarketController extends MskimRequestMapping {
+	
+	@RequestMapping("buyForm")
+	public String goBuyForm() {
+		return "member/buyForm";
+	}
+	@RequestMapping("cartForm")
+	public String goCartForm() {
+		return "member/cartForm";
+	}
+	@RequestMapping("marketForm")
+	public String goMarketForm() {
+		return "member/marketForm";
+	}
+
+	
+}
