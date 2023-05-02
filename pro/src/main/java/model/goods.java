459374b6feddfab7d79d68e1ad378ad179096a5d
ysl <akfh0279@naver.com>
2023-05-02 17:13:52 +0900

    20230502

---
+++ pro/src/main/java/model/goods.java
@@ -0,0 +1,38 @@
+package model;
+
+public class goods {
+	private int goodsCode;
+	private String goodsName;
+	private int goodsPrice;
+	private String goodsImg;
+	public int getGoodsCode() {
+		return goodsCode;
+	}
+	public void setGoodsCode(int goodsCode) {
+		this.goodsCode = goodsCode;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(int goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public String getGoodsImg() {
+		return goodsImg;
+	}
+	public void setGoodsImg(String goodsImg) {
+		this.goodsImg = goodsImg;
+	}
+	@Override
+	public String toString() {
+		return "goods [goodsCode=" + goodsCode + ", goodsName=" + goodsName + ", goodsPrice=" + goodsPrice
+				+ ", goodsImg=" + goodsImg + "]";
+	}
+
+}
